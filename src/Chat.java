28a04998fe771e4e933893993a6014baf2340b59
cxDoc <boctorbee@gmail.com>
2016-05-22 17:47:30 +0300

    Initial commit

---
+++ src/Chat.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+
+/**
+ * Created by cxDoctor on 22.05.2016.
+ */
+public class Chat {
+    public static void main (String[] args){
+        out.println("Hello World!");
+        String a = Met.userSays();
+        out.println("Hi, "+a);
+    }
+
+}
---
+++ src/Met.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by cxDoctor on 22.05.2016.
+ */
+public class Met {public static String userSays() {
+    Scanner sc = new Scanner(System.in);
+    String s1 = sc.nextLine();
+    return s1;
+}
+
+}
